6831e57555c159c276f307d78f0f6f9e11316af7
diego.silva.contractor <diego.silva.contractor@bbva.com>
2020-02-28 11:33:48 +0100

    Update TreeDetector class and test

---
+++ src/TreeDetector.py
@@ -0,0 +1,20 @@
+import cv2
+
+
+class Detector:
+
+    def __init__(self):
+        print("")
+
+    # *** PUBLIC *** #
+    def calculate_percentage(self, image):
+        percentage = 0.80
+        return percentage
+
+    def calculate_m2(self, image):
+        m2 = 90
+        return m2
+
+    def calculate_numer_trees(self, image):
+        n_trees = 120
+        return n_trees
--- tests/tests.py
+++ tests/tests.py
@@ -1,22 +1,62 @@
-from .context import src
+from src import TreeDetector
 import unittest
+import os
+import cv2
+
+
+constant_percentage = 0.75
+constant_km2 = 80
+constant_n_trees = 100
+path = "C:/Users/dsilvalo/PycharmProjects/AIVA-DAIA/images_test/"
 
 
 class Test(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        cls.detector = TreeDetector.Detector()
         print('Init class')
 
     def setUp(self):
         print('Init test')
 
-    def test_1(self):
-        print('Test 1')
+    def test_percentage(self):
+        """
+        Test for checking the percentage of tree mass
+        """
+        images = os.listdir(path)
+        for img in images:
+            i = cv2.imread(path + img)
+            percentage = self.detector.calculate_percentage(i)
+
+        self.assertGreater(percentage, constant_percentage)
+
+    def test_m2(self):
+        """
+        Test for checking the area of tree mass
+        """
+        images = os.listdir(path)
+        for img in images:
+            i = cv2.imread(path + img)
+            m2 = self.detector.calculate_m2(i)
+
+        self.assertGreater(m2, constant_km2)
+
+    def test_numer_trees(self):
+        """
+        Test for checking the number of trees
+        """
+        images = os.listdir(path)
+        for img in images:
+            i = cv2.imread(path + img)
+            n_trees = self.detector.calculate_numer_trees(i)
+
+        self.assertGreater(n_trees, constant_n_trees)
+
 
 if __name__ == '__main__':
     # Test sets
     tests1 = unittest.TestSuite()
-    tests1.addTest(Test('test_1'))
+    tests1.addTest(Test('test_rectangle'))
 
     # Test launch
     # unittest.TextTestRunner().run(tests1)
